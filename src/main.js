f06a0b879d02e34d21e7ea8695ce7ef628175668
muco <mucorolle@gmail.com>
2020-08-02 11:46:41 +0200

    refactor: rename variables names.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-const { join } = require("path");
-const { readFileSync, writeFileSync } = require("fs");
-const marked = require("marked");
+const { join } = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+const marked = require('marked');
 
-const markdownFile = join(process.cwd(), "src/post-test.md");
-const markdownContent = readFileSync(markdownFile, "utf8");
+const markdownFile = join(process.cwd(), 'src/post.md');
 
+const markdownContent = readFileSync(markdownFile, 'utf8');
 const htmlContent = marked(markdownContent);
 
-const htmlFile = markdownFile.slice(0, -3) + ".html";
-
-const response = writeFileSync(htmlFile, htmlContent);
-console.log(response);
+const htmlFile = markdownFile.slice(0, -3) + '.html';
+writeFileSync(htmlFile, htmlContent);
